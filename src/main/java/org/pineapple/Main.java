44bed41ecff73480f81004a6edfc3ff17163049c
KirilBurlakov <burlakovkiril19@gmail.com>
2019-12-20 17:46:13 +0200

    More modifications

--- src/main/java/org/pineapple/Main.java
+++ src/main/java/org/pineapple/Main.java
@@ -14,7 +14,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.net.InetAddress;
 
 @SpringBootApplication
 public class Main extends Application
